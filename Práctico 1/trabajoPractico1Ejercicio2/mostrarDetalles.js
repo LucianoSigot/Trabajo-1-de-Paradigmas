b3413aa5d2dbd9bac7f1bceac6a63dfcbee394c1
LucianoSigot <lucianosigot@gmail.com>
2024-10-05 09:55:58 -0300

    Delete Práctico 1/trabajoPractico1Ejercicio2 directory

--- Práctico 1/trabajoPractico1Ejercicio2/mostrarDetalles.js
+++
@@ -1,9 +0,0 @@
-export function mostrarDetalles(tarea) {//Funcion que muestra los detalles del arreglo 
-    console.log(`Título: ${tarea.titulo}`);
-    console.log(`Descripción: ${tarea.descripcion || 'Sin descripción'}`);
-    console.log(`Estado: ${tarea.estado}`);
-    console.log(`Fecha de Creación: ${tarea.fechaCreacion}`);
-    console.log(`Última Edición: ${tarea.fechaUltimaEdicion || 'No se ha editado'}`);
-    console.log(`Vencimiento: ${tarea.vencimiento || 'Sin vencimiento'}`);
-    console.log(`Dificultad: ${tarea.dificultad === 1 ? '⭐' : tarea.dificultad === 2 ? '⭐⭐' : '⭐⭐⭐'}`);
-}
