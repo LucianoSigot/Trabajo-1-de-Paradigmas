958dba9a0503b9bd3b1eb09eef1a7c9f25aebe80
LucianoSigot <lucianosigot@gmail.com>
2024-10-07 22:42:41 -0300

    Update editarTarea.js

--- Práctico 1/trabajoPractico1Ejercicio2/editarTarea.js
+++ Práctico 1/trabajoPractico1Ejercicio2/editarTarea.js
@@ -5,18 +5,18 @@ export function editarTarea(tarea) {//función que se encarga de editar las tare
     console.log("Editando tarea");
     
     let nuevoTitulo = prompt("Nuevo título (dejar en blanco para mantener el actual):");
-    if (nuevoTitulo) tarea.titulo = nuevoTitulo;
+    if (nuevoTitulo) tarea['titulo'] = nuevoTitulo;
 
     let nuevaDescripcion = prompt("Nueva descripción (dejar en blanco para mantener el actual):");
-    if (nuevaDescripcion) tarea.descripcion = nuevaDescripcion;
+    if (nuevaDescripcion) tarea['descripcion'] = nuevaDescripcion;
 
     let nuevoEstado = prompt("Nuevo estado (Pendiente, En curso, Terminada, Cancelada):");
-    while(nuevoEstado && !['pendiente','en curso','terminada'].includes(nuevoEstado.trim().toLowerCase())){
+    while(nuevoEstado && !['pendiente','en curso','terminada']){
       console.log("Ingrese una de los estados anteriores por pantalla");
       nuevoEstado=prompt("Nuevo estado: ");
     }
     
-    if (nuevoEstado) tarea.estado = nuevoEstado.trim().toLowerCase();
+    if (nuevoEstado) tarea['estado'] = nuevoEstado.trim().toLowerCase();
     let nuevaDificultad = prompt("Nueva dificultad (1: Fácil, 2: Medio, 3: Difícil):");
 
     while (nuevaDificultad && (isNaN(parseInt(nuevaDificultad)) || parseInt(nuevaDificultad) < 1 || parseInt(nuevaDificultad) > 3)) {
@@ -26,6 +26,6 @@ export function editarTarea(tarea) {//función que se encarga de editar las tare
 
     if(nuevaDificultad) tarea.dificultad=nuevaDificultad;
 
-    tarea.fechaUltimaEdicion = new Date();
+    tarea['fechaUltimaEdicion'] = new Date();
     console.log("Tarea editada correctamente");
 }
